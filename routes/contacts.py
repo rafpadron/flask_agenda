8c295b4a1d2afce7e4cde5e8446371894143dda6
rafpadron <rafaelpadron0212@gmail.com>
2024-12-19 10:43:14 -0300

    se inicializo la aplicacion y se crearon las rutas

---
+++ routes/contacts.py
@@ -0,0 +1,23 @@
+from flask import Blueprint, render_template
+
+contacts = Blueprint('contacts', __name__)
+
+@contacts.route('/')
+def home():
+    return render_template('index.html')
+
+@contacts.route('/new')
+def add_contact():
+    return "saving a contact"
+
+@contacts.route('/update')
+def update():
+    return "update a contact"
+
+@contacts.route('/about')
+def about():
+    return render_template('about.html')
+
+@contacts.route('/delete')
+def delete():
+    return "delete a contact"
