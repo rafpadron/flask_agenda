bfb2c94896cadf144263f005985686de519d1c9f
rafpadron <rafaelpadron0212@gmail.com>
2024-12-20 20:10:11 -0300

    se pusieron los estilos y se termino toda la parte de la agenda

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 from flask import Flask
 from routes.contacts import contacts
-
+from utils.db import db
+from config import DATABASE_CONNECTION_URI
 app = Flask(__name__)
+
+app.secret_key = "secret key"
+app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_CONNECTION_URI
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
+
 
 app.register_blueprint(contacts)
--- routes/contacts.py
+++ routes/contacts.py
@@ -1,23 +1,69 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for, flash
+from models.contact import Contact
+from utils.db import db
 
-contacts = Blueprint('contacts', __name__)
+contacts = Blueprint("contacts", __name__)
+
 
 @contacts.route('/')
-def home():
-    return render_template('index.html')
+def index():
+    contacts = Contact.query.all()
+    print(contacts)
+    return render_template('index.html', contacts=contacts)
 
-@contacts.route('/new')
+
+@contacts.route('/new', methods=['POST'])
 def add_contact():
-    return "saving a contact"
+    if request.method == 'POST':
 
-@contacts.route('/update')
-def update():
-    return "update a contact"
+        # receive data from the form
+        fullname = request.form['fullname']
+        email = request.form['email']
+        phone = request.form['phone']
 
-@contacts.route('/about')
+        # create a new Contact object
+        new_contact = Contact(fullname, email, phone)
+
+        # save the object into the database
+        db.session.add(new_contact)
+        db.session.commit()
+
+        flash('Contact added successfully!')
+
+        return redirect(url_for('contacts.index'))
+
+
+@contacts.route("/update/<string:id>", methods=["GET", "POST"])
+def update(id):
+    # get contact by Id
+    print(id)
+    contact = Contact.query.get(id)
+
+    if request.method == "POST":
+        contact.fullname = request.form['fullname']
+        contact.email = request.form['email']
+        contact.phone = request.form['phone']
+
+        db.session.commit()
+
+        flash('Contact updated successfully!')
+
+        return redirect(url_for('contacts.index'))
+
+    return render_template("update.html", contact=contact)
+
+
+@contacts.route("/delete/<id>", methods=["GET"])
+def delete(id):
+    contact = Contact.query.get(id)
+    db.session.delete(contact)
+    db.session.commit()
+
+    flash('Contact deleted successfully!')
+
+    return redirect(url_for('contacts.index'))
+
+
+@contacts.route("/about")
 def about():
-    return render_template('about.html')
-
-@contacts.route('/delete')
-def delete():
-    return "delete a contact"
+    return render_template("about.html")
